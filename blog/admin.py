3a5d651989de00ac78cce6017f8d19f936d7cb69
unknown <pspshyun98@gmail.com>
2020-04-13 15:17:00 +0900

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from blog.models import Category, Post
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
+from django.contrib import admin
+
+# Register your models here.
